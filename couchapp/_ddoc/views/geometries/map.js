9909db3d59ac09e556f05223b0e522760c0247c7
Pavel Nabutovsky <pavel.nabutovsky@quoininc.com>
2014-01-28 14:57:12 -0500

    Removing unused view.

--- couchapp/_ddoc/views/geometries/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) {
-    //Get all tows and their respective geometries.
-    //TODO: May need to change the schema and map a little in the future to support filtering by multiple states.
-    if (doc.type == 'town'){
-        emit(doc._id, doc.geometries);
-    }
-}
